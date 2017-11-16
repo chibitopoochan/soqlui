52bc1c32a9e8c67a46fcbc7e953afa9d775096d3
mametochoko <mametochoko@gmail.com>
2017-11-16 21:19:27 +0900

    Fixed #26

--- src/main/java/com/gmail/chibitopoochan/soqlui/initializer/parts/SOQLHistoryPartInitializer.java
+++ src/main/java/com/gmail/chibitopoochan/soqlui/initializer/parts/SOQLHistoryPartInitializer.java
@@ -4,22 +4,37 @@ import com.gmail.chibitopoochan.soqlui.controller.MainController;
 import com.gmail.chibitopoochan.soqlui.logic.SOQLHistoryLogic;
 import com.gmail.chibitopoochan.soqlui.model.SOQLHistory;
 
+import javafx.collections.FXCollections;
 import javafx.scene.control.ListView;
+import javafx.scene.control.TextField;
 
 public class SOQLHistoryPartInitializer implements PartsInitializer<MainController> {
 	private ListView<SOQLHistory> historyList;
 	private SOQLHistoryLogic logic;
+	private TextField historySearch;
 
 	@Override
 	public void setController(MainController controller) {
 		this.historyList = controller.getHistoryList();
 		this.logic = controller.getHistory();
+		this.historySearch = controller.getHistorySearch();
 	}
 
 	@Override
 	public void initialize() {
 		historyList.getItems().addAll(logic.getHistoryList());
 		historyList.setItems(historyList.getItems().sorted((i, j) -> -i.getCreatedDate().compareTo(j.getCreatedDate())));
+
+		// 履歴一覧の絞り込み
+		historySearch.textProperty().addListener(
+			(v, o, n) -> historyList.setItems(
+				FXCollections.observableArrayList(logic.getHistoryList()).
+				filtered(
+					t -> t.getQuery().toLowerCase().indexOf(n) > -1
+					|| n.length() == 0)
+			)
+		);
+
 	}
 
 }
